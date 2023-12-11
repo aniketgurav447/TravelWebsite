e317b9dac0ad02e8b61195db11291e6f3c676116
Aniket Satish Gurav <114214448+aniketgurav447@users.noreply.github.com>
2023-12-11 15:59:57 +0530

    Added all project files

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import Carousel from './components/Carousel';
+import Destinations from './Components/Destinations';
+import Footer from './Components/Footer';
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
+import Search from './components/Search';
+import Selects from './Components/Selects';
+
+
+
+
+
+function App() {
+  return (
+    <div>
+
+
+
+
+      <div>
+
+        <Navbar />
+        <Hero />
+        <Destinations />
+        <Search />
+        <Selects />
+
+        <Carousel />
+        <Footer />
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
